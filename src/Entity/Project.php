f434a546dd97450cf106e8190b50f6e7017910dc
Louis FREMEAUX <louis.fremeaux@icloud.com>
2026-01-14 17:55:41 +0100

    add projects details & text

--- src/Entity/Project.php
+++ src/Entity/Project.php
@@ -3,6 +3,8 @@
 namespace App\Entity;
 
 use App\Repository\ProjectRepository;
+use Doctrine\Common\Collections\ArrayCollection;
+use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 
 #[ORM\Entity(repositoryClass: ProjectRepository::class)]
@@ -24,6 +26,17 @@ class Project
 
     #[ORM\Column(length: 255, nullable: true)]
     private ?string $GithubLink = null;
+
+    /**
+     * @var Collection<int, Paragraphe>
+     */
+    #[ORM\OneToMany(targetEntity: Paragraphe::class, mappedBy: 'id_project')]
+    private Collection $paragraphes;
+
+    public function __construct()
+    {
+        $this->paragraphes = new ArrayCollection();
+    }
 
     public function getId(): ?int
     {
@@ -74,6 +87,36 @@ class Project
     public function setGithubLink(?string $GithubLink): static
     {
         $this->GithubLink = $GithubLink;
+
+        return $this;
+    }
+
+    /**
+     * @return Collection<int, Paragraphe>
+     */
+    public function getParagraphes(): Collection
+    {
+        return $this->paragraphes;
+    }
+
+    public function addParagraphe(Paragraphe $paragraphe): static
+    {
+        if (!$this->paragraphes->contains($paragraphe)) {
+            $this->paragraphes->add($paragraphe);
+            $paragraphe->setIdProject($this);
+        }
+
+        return $this;
+    }
+
+    public function removeParagraphe(Paragraphe $paragraphe): static
+    {
+        if ($this->paragraphes->removeElement($paragraphe)) {
+            // set the owning side to null (unless already changed)
+            if ($paragraphe->getIdProject() === $this) {
+                $paragraphe->setIdProject(null);
+            }
+        }
 
         return $this;
     }
